3618fe543b010f198b0b6ef090c2cc67b309b7fd
Angel Luna <angellunadeveloper@gmail.com>
2022-12-22 20:21:50 -0500

    fix top scene

--- src/pages/Home/hooks.js
+++ src/pages/Home/hooks.js
@@ -1,5 +1,5 @@
 function calculateTopScene (gameData) {
-    if (gameData.colorPlayer || gameData.playing || gameData.waitingGame || gameData.createRoom || gameData.roomFriend) {
+    if (gameData.colorPlayer || gameData.playing || gameData.waitingGame || gameData.createRoom || gameData.roomFriend || gameData.endGame) {
         return 0
     } else {
         return null;
